6e172f5297c78d02c70e885418d38f8fa0e5c2d0
kaukeshav <keshav.kau@gmail.com>
2020-02-28 15:36:26 +0530

    Updates

---
+++ precache-manifest.828271fa31013651c509e1d73a1ae571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34244db4a878ec1c8e2834c53b12e95f",
+    "url": "/monsters-rolo/index.html"
+  },
+  {
+    "revision": "f148bb345be67e327879",
+    "url": "/monsters-rolo/static/css/main.2a38121d.chunk.css"
+  },
+  {
+    "revision": "cf54fbab4f64ac2943a0",
+    "url": "/monsters-rolo/static/js/2.f635dc0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolo/static/js/2.f635dc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f148bb345be67e327879",
+    "url": "/monsters-rolo/static/js/main.d187186a.chunk.js"
+  },
+  {
+    "revision": "6601762fdde5041434e2",
+    "url": "/monsters-rolo/static/js/runtime-main.543cc763.js"
+  }
+]);
